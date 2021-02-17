80c9cf16fd7ba9c472fef13301b1ba3afd73014d
Emma Dyer <ecdyer@cs.uchicago.edu>
2021-02-17 07:43:45 -0600

    get_parks() creates a dictionary of all the addresses and zip codes of Chicago parks, with non-repeating addresses

--- parks_scrape.py
+++ parks_scrape.py
@@ -74,11 +74,11 @@ def find_parks_nested():
 
 
 '''
-    with open("data.csv", "w", newline="") as csv_file:
-    cols = ["id","first_name","last_name","email","gender"] 
-    writer = csv.DictWriter(csv_file, fieldnames=cols)
-    writer.writeheader()
-    writer.writerows(data)
+    with open("all_parks.csv", "w", newline="") as csv_file:
+        cols = ["address", "zipcode"] 
+        writer = csv.DictWriter(csv_file, fieldnames=cols)
+        writer.writeheader()
+        writer.writerows(p)
 '''
 
 
@@ -101,49 +101,30 @@ def find_parks():
         park_text = park_page.text
         soup = bs4(park_text, 'html.parser')
         park_address = soup.find_all("p", class_="address", translate="no")
-        soup = bs4(park_text, 'html.parser')
-        park_name_soup = soup.find_all("a", href=re.compile(r"^/parks-facilities/"))
-        found_park = False
-        park_names = []
-        for park in park_name_soup:
-            a_tag = park["href"]
-            if a_tag == "/parks-facilities/a-z":
-                found_park = True
-                continue
-            if found_park:
-                park_ref = park["href"]
-                park_str = re.search(r"(?<=/parks\-facilities/).*[a-z0-9]", park_ref)
-                park_str = park_str.group(0)
-                park_str = park_str.replace("-", " ")
-                park_str = string.capwords(park_str)
-                park_names.append(park_str)
-        #print("Park Names:", len(park_names))
+        added_addrs = set()
         all_addrs = []
+        all_zip = []
         for addr in park_address:
             park_addr = []
             addr = addr.text
             addr_lst = addr.split()
-            if len(addr_lst) > 0:
-                zip_code = addr_lst[-1]
-                addr_lst = addr_lst[:-1]
+            if len(addr_lst) == 0:
+                continue
+            zip_code = addr_lst[-1]
+            addr_lst = addr_lst[:-3]
             for word in addr_lst:
-                if word != "Chicago," \
-                    and word != "IL":
-                    park_addr.append(word)
+                park_addr.append(word)
             address = " ".join(park_addr)
             address = address.replace(".", "")
             all_addrs.append(address)
-        #print("Addresses:", len(all_addrs))  
-        i = 0
-        for addr in all_addrs:
-            if len(park_names) < len(all_addrs):
-                continue
-            park_dict = dict()
-            park_dict["name"] = park_names[i]
-            park_dict["address"] = address
-            park_dict["zipcode"] = zip_code
-            i +=1
-            all_park_facil.append(park_dict)
+            all_zip.append(zip_code)   
+        for i, addr in enumerate(all_addrs):
+            if addr not in added_addrs:
+                park_dict = dict()
+                park_dict["address"] = addr
+                park_dict["zipcode"] = all_zip[i]
+                all_park_facil.append(park_dict)
+                added_addrs.add(addr)
         page_num = "?page=" + str(n)
     return all_park_facil
 
