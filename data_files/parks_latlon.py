16b61777fa93acdc5f8d48f85cde21080d95f53f
Emma Dyer <ecdyer@sbcglobal.net>
2021-03-01 21:51:14 -0600

    python script to get the lattitude and longitude of parks

--- data_files/parks_latlon.py
+++ data_files/parks_latlon.py
@@ -1,7 +1,22 @@
-#%%
+
 import geocoder
 import pandas as pd
-# %%
+'''
+Get the lattitude and longitude of Chicago parks using the 
+MapQuest API (with Emma's API key)
+'''
 parks = pd.read_csv("all_parks.csv")
-print(len(parks))
-# %%
+address = parks["address"]
+zip_code = parks["zipcode"]
+lat_lst = []
+lon_lst = []
+len(parks)
+
+for i in range(len(parks)):
+    print(i)
+    full_address = address.iloc[i] + ", " + str(zip_code[i])
+    g = geocoder.mapquest(full_address, key = "mmcN5Afp3QejG7fOZ0ljDyhrneaqeazD")
+    lat_lst.append(g.lat)
+    lon_lst.append(g.lng)
+
+
