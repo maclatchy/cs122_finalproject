d8bd76e43d147eed9578630173fdf4a740114a18
Sean MacLatchy <maclatchy@cs.uchicago.edu>
2021-03-09 12:25:34 -0600

    progress

--- data_gathering/user_interface.py
+++ data_gathering/user_interface.py
@@ -1,1 +1,29 @@
 # User interface
+
+print("\nWelcome to the Spike-Protein's final project!\n")
+print("\nOur goal is to help you find the ideal Chicago neighborhood for"
+      " you to live!\n")
+print("\nPlease input a whole number ranged [0, 5] for all prompts with 0 being"
+      " the least important and 5 being the most important.\n")
+
+preference_dict = dict()
+
+error_value = "\nPlease input a whole number ranged [0, 5]\n"
+
+
+while True:
+    school = input("How important is proximity to schools for you? ")
+    try:
+        school = int(school)
+    except:
+        print("\nYou did not enter a valid whole number.\n")
+        continue
+    if school > 5 or school < 0:
+        print(error_value)
+        continue
+    else:
+        preference_dict['school'] = school
+        break
+
+print(preference_dict)
+
