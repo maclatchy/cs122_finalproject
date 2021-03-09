968b65562ae68ea5e367428b90730e7a4052c159
Sean MacLatchy <maclatchy@cs.uchicago.edu>
2021-03-08 20:31:48 -0600

    cleaning

--- data_gathering/mapping.py
+++ data_gathering/mapping.py
@@ -6,21 +6,21 @@ from shapely.geometry import Point
 import contextily as ctx
 
 # Load base layers
-zip_gdf = gpd.read_file("/data_files/chicago_zip_tracts.shp")
+zip_gdf = gpd.read_file("../data_files/chicago_zip_tracts.shp")
 zip_gdf.zip = zip_gdf.zip.astype(int)
-census_gdf = gpd.read_file("/data_files/census_tract_boundaries.shp")
-neighbor_gdf = gpd.read_file("/data_files/Neighborhoods_2012b.shp")
+zip_gdf = zip_gdf.drop(['objectid'], axis=1)
+#census_gdf = gpd.read_file("../data_files/census_tract_boundaries.shp")
+#neighbor_gdf = gpd.read_file("../data_files/Neighborhoods_2012b.shp")
 
 # Grocery stores
-grocery = pd.read_csv("/Users/seanmaclatchy/Desktop/CS122/project/grocery_stores.csv")
+grocery = pd.read_csv("../data_files/grocery_stores.csv", usecols=[6, 14, 15])
 grocery.columns = grocery.columns.str.lower()
-grocery.rename(columns = {'zip code':'zip'}, inplace = True) 
+grocery.rename(columns={'zip code': 'zip'}, inplace=True)
 geometry = [Point(xy) for xy in zip(grocery.longitude, grocery.latitude)]
-grocery = grocery.drop(['longitude', 'latitude'], axis=1)
 grocery = gpd.GeoDataFrame(grocery, crs="EPSG:4326", geometry=geometry)
 
 # Health centers
-health = gpd.read_file('/data_files/health_centers.csv')
+health = gpd.read_file('../data_files/health_centers.csv')
 health.columns = health.columns.str.lower().str.strip()
 health[['zip']] = health.address.str.extract(r'(\d{5})')
 health[['lat']] = health.address.str.split(
@@ -39,7 +39,7 @@ health.crs = 'EPSG:4326'
 health = health.drop([41])
 
 # Libraries
-library = gpd.read_file('/data_files/library.csv')
+library = gpd.read_file('../data_files/library.csv')
 library.columns = library.columns.str.lower().str.strip()
 library[['lat', 'lon']] = library.location.str.split(expand=True)
 library.lat = library.lat.str.strip('(').str.strip(',')
@@ -54,7 +54,7 @@ library.geometry = [Point(xy) for xy in zip(library.lon, library.lat)]
 library.crs = 'EPSG:4326'
 
 # Parks
-parks = gpd.read_file('/data_files/new_parks.csv')
+parks = gpd.read_file('../data_files/new_parks.csv')
 parks.columns = parks.columns.str.lower().str.strip()
 parks.rename(columns={'zipcode': 'zip', 'lattitude': 'lat',
                       'longitude': 'lon'}, inplace=True)
@@ -67,7 +67,7 @@ parks.crs = 'EPSG:4326'
 parks = parks.drop([1451, 372])
 
 # Schools
-schools = pd.read_csv('/data_files/CPS.csv', usecols=[16, 19, 67, 68])
+schools = pd.read_csv('../data_files/CPS.csv', usecols=[16, 19, 67, 68])
 schools.columns = schools.columns.str.lower().str.strip()
 schools.rename(columns={'school_latitude': 'lat',
                         'school_longitude': 'lon'}, inplace=True)
@@ -78,24 +78,32 @@ geometry = [Point(xy) for xy in zip(schools.lon, schools.lat)]
 schools = gpd.GeoDataFrame(schools, crs='EPSG:4326', geometry=geometry)
 
 # CTA rail stations
-cta_rail = gpd.read_file('/data_files/CTA_RailStations.shp')
+cta_rail = gpd.read_file('../data_files/CTA_RailStations.shp')
+cta_rail_zip = pd.read_csv('../data_files/cta_railstation_zips.csv')
+cta_rail = pd.concat([cta_rail, cta_rail_zip], axis=1)
 cta_rail.columns = cta_rail.columns.str.lower().str.strip()
-to_drop = ['station_id', 'longname', 'ada', 'pknrd',
-           'gtfs', 'point_x', 'point_y']
+cta_rail.rename(columns={'zipcode': 'zip'}, inplace=True)
+to_drop = ['station_id', 'longname', 'ada', 'pknrd', 'gtfs',
+           'point_x', 'point_y']
 cta_rail.drop(columns=to_drop, inplace=True)
 cta_rail = cta_rail.to_crs('EPSG:4326')
 cta_rail
 
 # CTA bus stops
-cta_bus = gpd.read_file('/data_files/CTA_BusStops.shp')
+cta_bus = gpd.read_file('../data_files/CTA_BusStops.shp')
+cta_bus_zip = pd.read_csv('../data_files/cta_bus_zips.csv')
+cta_bus = pd.concat([cta_bus, cta_bus_zip], axis=1)
 cta_bus.columns = cta_bus.columns.str.lower().str.strip()
+cta_bus.rename(columns={'zipcode': 'zip'}, inplace=True)
 cta_bus = cta_bus.loc[cta_bus.city == 'CHICAGO']
-to_drop = ['systemstop', 'owlroutes', 'city', 'status', 'public_nam']
+to_drop = ['systemstop', 'objectid', 'street', 'cross_st', 'point_x',
+           'point_y', 'owlroutes', 'city', 'status', 'public_nam', 'dir',
+           'routesstpg', 'pos']
 cta_bus.drop(columns=to_drop, inplace=True)
 cta_bus
 
-# Get zip and points function
-def get_zip_lib(zip_code):
+
+def community_profile_map(zip_code):
     '''
     Input:
       zip (int): ZIP code
@@ -103,44 +111,22 @@ def get_zip_lib(zip_code):
     Output:
       Map of zip code with library locations
     '''
-    fig, ax = plt.subplots(figsize=(10, 10))
-    library.loc[library.zip == zip_code].plot(ax=ax, color='red')
-    zip_gdf.loc[zip_gdf.zip == zip_code].geometry.boundary.plot(ax=ax, color='black')
-    health.loc[health.zip == zip_code].plot(ax=ax, color='blue')
-    grocery.loc[grocery.zip == zip_code].plot(ax=ax, color='green')
-    plt.show()
-    return
-
-def community_profile_map(zip_code):
-  '''
-  Input:
-    zip (int): ZIP code
-
-  Output:
-    Map of zip code with library locations
-  '''
-  # Point layer of libraries
-  ax0 = library.loc[library.zip == zip_code].plot(color='green', 
+    ax0 = library.loc[library.zip==zip_code].plot(color='green',
                                                   figsize=(10, 10))
-  # Point layer of health centers
-  ax1 = health.loc[health.zip == zip_code].plot(ax=ax0, 
-                                                color='red', 
+    ax1 = health.loc[health.zip==zip_code].plot(ax=ax0,
+                                                color='red',
                                                 figsize=(10, 10))
-  # Point layer of grocery stores
-  ax2 = grocery.loc[grocery.zip == zip_code].plot(ax=ax1, 
-                                                  color='blue', 
+    ax2 = grocery.loc[grocery.zip==zip_code].plot(ax=ax1,
+                                                  color='blue',
                                                   figsize=(10, 10))
-  # Point layer for park location
-  ax3 = parks.loc[parks.zip == zip_code].plot(ax=ax2, 
-                                              color='blue', 
+    ax3 = parks.loc[parks.zip==zip_code].plot(ax=ax2,
+                                              color='blue',
                                               figsize=(10, 10))
-  # Polygon layer of zip codes
-  final_ax = zip_gdf.loc[zip_gdf.zip == zip_code].boundary.plot(ax=ax3, 
-                                                                color= 'black', 
+    ax4 = schools.loc[schools.zip==zip_code].plot(ax=ax3,
+                                                  color='yellow',
+                                                  figsize=(10, 10))
+    final_ax = zip_gdf.loc[zip_gdf.zip==zip_code].boundary.plot(ax=ax4,
+                                                                color='black',
                                                                 figsize=(10, 10))
-  # Streetview basemap
-  ctx.add_basemap(final_ax, crs=zip_gdf.crs.to_string())
-  return 
-
-community_profile_map(60615)
-
+    ctx.add_basemap(final_ax, crs=zip_gdf.crs.to_string())
+    return
