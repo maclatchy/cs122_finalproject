aed2ad49c62a92993857dfe99509a57f639f584a
Sean MacLatchy <maclatchy@cs.uchicago.edu>
2021-03-09 12:34:11 -0600

    I've got the general format I just need to make it a function

--- data_gathering/user_interface.py
+++ data_gathering/user_interface.py
@@ -1,22 +1,23 @@
 # User interface
 
-print("\nWelcome to the Spike-Protein's final project!\n")
-print("\nOur goal is to help you find the ideal Chicago neighborhood for"
-      " you to live!\n")
-print("\nPlease input a whole number ranged [0, 5] for all prompts with 0 being"
-      " the least important and 5 being the most important.\n")
+print("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"
+      "\nWelcome to the Spike-Protein's final project!\n")
+print("\nOur goal is to help you find the ideal Chicago neighborhood for you"
+      "\nto live!\n")
+print("\nPlease input a whole number ranged [0, 5] for all prompts, with 0"
+      "\nbeing the least important and 5 being the most important.\n"
+      "\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n")
 
 preference_dict = dict()
 
 error_value = "\nPlease input a whole number ranged [0, 5]\n"
 
-
 while True:
-    school = input("How important is proximity to schools for you? ")
+    school = input("How important is proximity to schools for you? \n")
     try:
         school = int(school)
     except:
-        print("\nYou did not enter a valid whole number.\n")
+        print("\nYou did not enter a valid whole number.")
         continue
     if school > 5 or school < 0:
         print(error_value)
@@ -24,6 +25,6 @@ while True:
     else:
         preference_dict['school'] = school
         break
-
+print('\nThank you!\n')
 print(preference_dict)
 
