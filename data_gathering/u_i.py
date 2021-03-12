11b00bf70ea16f6aa361e2b5cd6644b88f3ca0f6
Sean MacLatchy <maclatchy@cs.uchicago.edu>
2021-03-12 11:08:06 -0600

    removed property from mapping function

--- data_gathering/u_i.py
+++ data_gathering/u_i.py
@@ -108,14 +108,12 @@ while True:
         print("\n We didn't recognize that as a Chicago zip code")
         continue
 
-properties = p_object.get_prop_geom(price, beds)
-
 while True:
     response = input('\nWould you like to see a map of {}? [y or n]\n'.format(str(zip_inquiry)))
     if 'y' == response.lower():
         zip_inquiry = int(zip_inquiry)
         params = ['all']
-        mapping.community_profile_map(zip_inquiry, params, properties)
+        mapping.community_profile_map(zip_inquiry, params)
         while True:
             new_response = input('\nWould you like to see specific amenities [y or n]?\n')
             if 'y' == new_response.lower() or 'n' == new_response.lower():
@@ -143,7 +141,7 @@ while True:
     print("\nPlease type 'done' when you don't want to see any additional amenities.\n")
     num = input('\n1=grocery, 2=health, 3=parks, 4=schools, 5=cta_rail, 6=library\n')
     if num.lower() == 'done':
-        mapping.community_profile_map(zip_inquiry, list(param_s), properties)
+        mapping.community_profile_map(zip_inquiry, list(param_s))
         break 
     try:
         num = int(num)
