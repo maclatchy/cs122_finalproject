294e4c516e3f627e8acfac997656f8566a95f61d
Matt Ludwig <mwl@uchicago.edu>
2021-03-12 08:57:03 -0600

    added function headers

--- data_gathering/zip_recommendation.py
+++ data_gathering/zip_recommendation.py
@@ -4,15 +4,21 @@ import numpy as np
 df = pd.read_csv("../data_files/z_score_counts.csv")
 
 def weighting(zipcode, user_weights):
-    #given a particular zip code calculate score from user weights
+    '''
+    Get score for each category and zip code given user input and z scores
+    '''
     keys = list(user_weights.keys())
     score = 0
     for i in range(len(keys)):
         score += zipcode[i+8]*user_weights.get(keys[i])
     return score
-#multiply z_crimes by -1 because high z_score on crimes means more than average
+
 
 def get_sorted_weights(user_weights):
+    '''
+    Create a new dataframe of score by zipcode for ranking purposes
+    '''
+    #multiply z_crimes by -1 because high z_score on crimes means more than average
     user_weights["z_crimes"] *= -1
     zips = []
     scores = []
